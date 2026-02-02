cc3682c941527befbd8fd6f8501eac37430e1926
VK-MONISH KUMAR <139607720+VK-Monish@users.noreply.github.com>
2026-02-02 21:13:03 +0530

    Day_02

---
+++ day_02/Basic_hello/modules.order
@@ -0,0 +1,1 @@
+/home/monish/Documents/ldd/day_02/Basic_hello/basic.o
---
+++ day_02/module_param/module_param.c
@@ -0,0 +1,66 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+
+static int valueETX;
+static int arr_valueETX[4];
+static char *nameETX;
+static int cb_valueETX;
+
+module_param(valueETX, int, S_IRWXU);
+module_param(nameETX, charp, S_IRWXU);
+module_param_array(arr_valueETX, int, NULL, S_IRWXU);
+
+MODULE_PARM_DESC(valueETX, "An integer value");
+MODULE_PARM_DESC(nameETX, "Name string");
+MODULE_PARM_DESC(arr_valueETX, "Integer array");
+MODULE_PARM_DESC(cb_valueETX, "Callback integer");
+
+static int notify_param(const char *val, const struct kernel_param *kp)
+{
+    int res = param_set_int(val, kp);
+    if (res)
+        return res;
+
+    if (cb_valueETX < 0)
+        return -EINVAL;
+
+    pr_info("Callback called, cb_valueETX = %d\n", cb_valueETX);
+    return 0;
+}
+
+static const struct kernel_param_ops my_param_ops = {
+    .set = notify_param,
+    .get = param_get_int,
+};
+
+module_param_cb(cb_valueETX, &my_param_ops, &cb_valueETX, S_IRWXU);
+
+static int __init call_back(void)
+{
+    int i;
+
+    pr_info("valueETX = %d\n", valueETX);
+    pr_info("cb_valueETX = %d\n", cb_valueETX);
+    pr_info("nameETX = %s\n", nameETX ? nameETX : "NULL");
+
+    for (i = 0; i < ARRAY_SIZE(arr_valueETX); i++)
+        pr_info("arr_valueETX[%d] = %d\n", i, arr_valueETX[i]);
+
+    pr_info("Kernel module inserted successfully\n");
+    return 0;
+}
+
+static void __exit n_call_back(void)
+{
+    pr_info("Kernel module removed successfully\n");
+}
+
+module_init(call_back);
+module_exit(n_call_back);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Monish Kumar");
+MODULE_DESCRIPTION("Module parameter callback example");
+
